3d31add1dd779f4625b9b139e608e2ac0ccd58c0
Baron Hartono <baronhartono@gmail.com>
2019-10-01 10:43:31 +0700

    Commit

--- index.js
+++ index.js
@@ -1,5 +1,7 @@
 const express = require('express')
 const mysql = require('mysql')
+const bodyParser = require('body-parser')
+const cors = require('cors')
 
 const app = express()
 const port = process.env.PORT || 1997
@@ -10,9 +12,13 @@ const db = mysql.createConnection({
     password: 'abc123',
     database: 'tokoberkah',
     port: 3306,
-    timezone: 'UTC'
+    // multipleStatements: true
+    // timezone: 'UTC'
 })
 
+app.use(bodyParser.json())
+app.use(cors())
+app.use(express.static('public'))
 
 app.get('/', (req,res) => {
     res.status(200).send('<h1>Welcome To our API</h1>')
@@ -31,15 +37,16 @@ app.get('/getkota', (req,res) => {
         res.status(200).send(results)
     })
 })
-
+// http://localhost:1997/getkota/bebas/test/halo/4124/testing
 // app.get('/getkota/:idu/test/halo/:hello/:coba', (req,res) => {
 //     console.log(req.params)
 //     res.status(200).send('<h1>Halo Bro</h1>')
 // })
 
 app.get('/getkota/:id', (req,res) => {
-    var sql =`SELECT * FROM kota WHERE id=${req.params.id};`;
+    var sql =`SELECT * FROM kota WHERE id=${db.escape(req.params.id)};`;
 
+    console.log(sql)
     db.query(sql, (err,results) => {
         if(err) {
             // console.log(err)
@@ -54,8 +61,10 @@ app.get('/gettoko', (req,res) => {
     var nama = req.query.nama || '';
     var alamat = req.query.alamat || '';
     
-    var sql =`SELECT * FROM toko 
-                WHERE nama LIKE '%${nama}%' 
+    var sql =`SELECT t.*, k.nama as namaKota FROM toko t
+                JOIN kota k
+                ON t.kotaId = k.id
+                WHERE t.nama LIKE '%${nama}%' 
                 AND alamat LIKE '%${alamat}%'`;
     
     if(req.query.incmin) {
@@ -79,7 +88,7 @@ app.get('/gettoko', (req,res) => {
             // console.log(err)
             return res.status(500).send(err)
         }
-
+        
         res.status(200).send(results)
     })
 })
@@ -90,6 +99,102 @@ app.get('/gettoko/:id', (req,res) => {
     db.query(sql, (err,results) => {
         if(err) {
             // console.log(err)
+            return res.status(500).send(err)
+        }
+
+        res.status(200).send(results)
+    })
+})
+
+app.post('/addkota', (req,res) => {
+    var kota = req.body.insertkota;
+    
+    if(kota) {
+        var sql = `INSERT INTO kota (nama) values ? `
+       
+        db.query(sql, [kota], (err, results) => {
+            if(err) {
+                return res.status(500).send(err)
+            }
+            sql = `SELECT * from kota;`
+            
+            db.query(sql, (err, results) => {
+                if(err) return res.status(500).send(err)
+
+                res.status(200).send(results)
+            })
+        })
+    }
+    else {
+        res.status(500).send('Tolong isi query insertkota')
+    }
+})
+
+app.put('/editkota/:id', (req,res) => {
+    var data = req.body;
+    var sql = `UPDATE kota SET ? WHERE id = ${req.params.id}`
+       
+    db.query(sql, data, (err, results) => {
+        if(err) {
+            return res.status(500).send(err)
+        }
+
+        sql = `SELECT * from kota;`
+        db.query(sql, (err,results1) => {
+            if(err) return res.status(500).send(err)
+
+            res.status(200).send(results1)
+        })
+    })
+})
+
+app.delete('/deletekota/:id', (req,res) => {
+    var sql = `DELETE FROM kota WHERE id = ${req.params.id}`
+       
+    db.query(sql, (err, results) => {
+        if(err) {
+            return res.status(500).send(err)
+        }
+
+        res.status(200).send(results)
+    })
+})
+
+app.post('/addtoko', (req,res) => {
+    var newToko = req.body;
+    console.log(newToko)
+    if(newToko) {
+        var sql = `INSERT INTO toko SET ? `
+       
+        db.query(sql, newToko, (err, results) => {
+            if(err) {
+                return res.status(500).send(err)
+            }
+
+            res.status(200).send(results)
+        })
+    }
+    else {
+        res.status(500).send('Tolong kasih Body')
+    }
+})
+
+app.delete('/deletetoko/:id', (req,res) => {
+    var sql = `DELETE FROM toko WHERE id = ${db.escape(req.params.id)}`
+       
+    db.query(sql, (err, results) => {
+        if(err) {
+            return res.status(500).send(err)
+        }
+
+        res.status(200).send(results)
+    })
+})
+
+app.put('/edittoko/:id', (req,res) => {
+    var sql = `UPDATE toko SET ? WHERE id = ${req.params.id};`;
+    db.query(sql, req.body, (err,results) => {
+        if(err) {
             return res.status(500).send(err)
         }
 
